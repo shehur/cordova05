bf42610ff112911807b67db943539a1de87c8898
shehur <shehur5727@gmail.com>
2018-10-29 00:33:18 -0300

    update 01

---
+++ www/js/index.js
@@ -0,0 +1,20 @@
+﻿var app = {
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    receivedEvent: function(id) {
+        var botao = document.getElementById('botao');
+        var print = document.getElementById('print');
+        botao.addEventListener('click', () => {
+            print.innerText = 'Botão acionado!';
+            alert('Botão acionado!');
+        });
+    }
+};
+
+app.initialize();
